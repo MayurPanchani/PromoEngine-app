5fb31e04bb4763aab092256466a361ee5ba46c36
MayurPanchani <mayurpanchani1989@yahoo.com>
2020-09-12 23:22:58 +0530

    Add console to test Promotion engine and add Promotion engine descriptin in Read me file

---
+++ PromotionEngineConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using PromotionEngine.Entities;
+using PromotionEngine.Repository;
+using System;
+using System.Linq;
+
+namespace PromotionEngineConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Promotion Engine Console !");
+            CalculatePromotions();
+        }
+
+
+        public static void CalculatePromotions()
+        {
+            ProductRepository productContext = new ProductRepository();
+            var products = productContext.Products;         
+
+            PromotionRepository promotionDB = new PromotionRepository();
+            var activePromotionList = promotionDB.GetActivePromotions();
+
+            Cart objCart = new Cart();
+
+            objCart.AddProduct(products.First(p => p.SKU == "A"), 3);
+            objCart.AddProduct(products.First(p => p.SKU == "B"), 5);
+            objCart.AddProduct(products.First(p => p.SKU == "C"), 1);
+            objCart.AddProduct(products.First(p => p.SKU == "D"), 1);
+
+
+            objCart.LoadPromotions(activePromotionList);
+
+            var result = objCart.CalculatePromotionOnCardValue();
+
+            
+
+            Console.WriteLine(string.Format("Total:{0}", result));
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PromoEngine-app
 Promotion Engine API for a checkout process
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+As per the requirement Created Promotion Engine project with below features
+
+- We can add/update new promotion in to the system, and we can make active or inactive promotions. This can be manage using PromotionRepository entity. For now I didnt created front end to manage promotions in database. So all the mentined Promotions are active by default in system
+
+- We can add/update products in the system. But for testing purpose we have 4 different types of products available in the system.
+
+- System can applies all the promotions at a time 
